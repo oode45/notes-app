709218beba0496e585414f3a9faabc92197dc71d
oode45 <55568515+oode45@users.noreply.github.com>
2021-06-08 21:47:50 +0600

    config and axios settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from "react"
+import {useSelector} from "react-redux"
+import {Redirect, Route, Switch} from "react-router-dom"
+import Layout from "./UI/Layout"
+import Register from "./containers/registrationForm"
+import Login from "./components/logInForm"
+import NotesPage from "./containers/NotesPage";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+const App = () => {
+    const user = useSelector((state) => state.users.user)
+
+    return (
+        <Layout>
+            <Switch>
+                <Route path='/' exact component={NotesPage}/>
+                <Route path='/register' component={Register}/>
+                <Route path='/login' component={Login}/>
+            </Switch>
+        </Layout>
+    )
 }
 
-export default App;
+export default App
---
+++ frontend/src/config.js
@@ -0,0 +1,2 @@
+export const faceBookId = process.env.REACT_APP_FACEBOOK_APP_ID
+export const faceBookSecret = process.env.REACT_APP_FACEBOOK_SECRET_KEY
