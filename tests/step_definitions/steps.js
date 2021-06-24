5d6a4a758a40e0c19285acdded90c952daa9b1fa
oode45 <55568515+oode45@users.noreply.github.com>
2021-06-24 20:15:22 +0600

    add auth features and steps

--- tests/step_definitions/steps.js
+++ tests/step_definitions/steps.js
@@ -1,3 +1,32 @@
 const { I } = inject();
 // Add in your custom step files
 
+
+Given('я нахожусь на странице регистрации', () => {
+    I.amOnPage('/register')
+});
+
+Given('я нахожусь на странице входа', () => {
+    I.amOnPage('/login')
+});
+
+When('я ввожу в форму данные:', (table) => {
+    for (const id in table.rows) {
+        if (id < 1) {
+            continue;
+        }
+        const cells = table.rows[id].cells
+        const field = cells[0].value;
+        const value = cells[1].value;
+        I.fillField(field, value)
+    }
+
+});
+
+When('нажимаю на кнопку {string}', (name) => {
+    I.click(`//button//*[contains(text(),'${name}')]/..`)
+});
+
+Then('вижу текст {string}', () => {
+    I.see(`successful`)
+});
