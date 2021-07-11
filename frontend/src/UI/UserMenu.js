4aa24af8880856b4352d750bd21acd64473de707
oode45 <55568515+oode45@users.noreply.github.com>
2021-07-11 14:15:58 +0600

    optimized imports

--- frontend/src/UI/UserMenu.js
+++ frontend/src/UI/UserMenu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {Button} from "@material-ui/core";
 import {logoutRequest} from "../store/actions/userActions";
 import {useDispatch} from "react-redux";
--- frontend/src/components/inputForm.js
+++ frontend/src/components/inputForm.js
@@ -2,7 +2,6 @@ import Box from '@material-ui/core/Box';
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {useDispatch} from "react-redux";
 import React from "react";
 import Container from "@material-ui/core/Container";
 
